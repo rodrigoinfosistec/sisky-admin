c5dd94eb5a483f873cf0e99c2ca058359783a4f7
rodrigoinfosistec <rodrigo.infosistec@gmail.com>
2026-07-20 12:40:37 -0300

    feat: add NextTopLoader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/theme-provider";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NextTopLoader color="#111" showSpinner={false} />
           {children}
           <Toaster richColors position="top-right" />
         </ThemeProvider>
